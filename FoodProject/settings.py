7154231b3344232a374fd2843f39e37ccef0fe8c
GauriKothekarnetsmartz <160697991+GauriKothekarnetsmartz@users.noreply.github.com>
2024-03-19 12:08:22 +0530

    Update settings.py

--- FoodProject/settings.py
+++ FoodProject/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
-ALLOWED_HOSTS = ['localhost' , '0.0.0.0']
 
 
 # Application definition
